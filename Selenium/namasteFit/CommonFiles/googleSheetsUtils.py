1d6058e6e12722618332babb3843beb4d16ed033
Nelly Nkong <nkongenelly94@gmail.com>
2020-08-21 14:23:25 +0530

    multibrowser testing

--- Selenium/namasteFit/CommonFiles/googleSheetsUtils.py
+++ Selenium/namasteFit/CommonFiles/googleSheetsUtils.py
@@ -1,21 +1,67 @@
-import openpyxl as opx
+import sys
+import os
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from namasteFit.TestServer.Locators.locators import Locators
+sys.path.append(os.path.join(os.path.dirname(__file__), "...", "..."))
+
 
 def getRowCount(file, client):
     sheet = client.open(file).sheet1
     return len(sheet.get_all_values())
 
+
 def getColumnCount(file, client):
     sheet = client.open(file).sheet1
     return len(sheet.get_all_values()[0])
+
 
 def readData(file, client, rowno, columno):
     sheet = client.open(file).sheet1
     return sheet.cell(rowno, columno).value
 
+
 def writeData(file, client, rowno, columno, data):
     sheet = client.open(file).sheet1
     sheet.update_cell(rowno, columno, data)
 
-def appendData(file,client,data):
+
+def appendData(file, client, data):
     sheet = client.open(file).sheet1
     sheet.append_row(data)
+
+
+def write_results(TestResults, browser, user_story):
+    written = False
+    print("3")
+    print(TestResults)
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(Locators.credentials, Locators.scope)
+    client = gspread.authorize(credentials)
+    max_rows = getRowCount(Locators.userStoriesTestCases, client)
+
+    if browser == "Webdriver.Firefox":
+        column = 3
+        new_row = [user_story, "", TestResults, "", "", ""]
+        addUnderBrowserTitle(max_rows, client, column, new_row)
+
+    elif browser == "Webdriver.Chrome":
+        column = 4
+        new_row = [user_story, "", "", "TestResults", "", ""]
+        addUnderBrowserTitle(max_rows, client, column, new_row)
+
+    elif browser == "Webdriver.Edge":
+        column = 5
+        new_row = [user_story, "", "", "", TestResults, ""]
+        addUnderBrowserTitle(max_rows, client, column, new_row, TestResults, user_story)
+
+
+def addUnderBrowserTitle(max_rows, client, col, new_row, TestResults, user_story):
+    for row in range(max_rows + 1):
+        print(row)
+        row_value = readData(Locators.userStoriesTestCases, client, row + 1, col)
+        if row_value == user_story:
+            writeData(Locators.userStoriesTestCases, client, row + 1, col, TestResults)
+            written = True
+
+    if written == False:
+        appendData(Locators.userStoriesTestCases, client, new_row)
--- Selenium/namasteFit/TestServer/UnitTests/107/tet_107refresh_page.py
+++ Selenium/namasteFit/TestServer/UnitTests/107/tet_107refresh_page.py
@@ -19,94 +19,71 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 TestResults = ""
+drivers = []
+user_story = "107"
+
 
 class LoginUnitTest(unittest.TestCase):
     driver = None
 
     @classmethod
     def setUpClass(cls):
-        cls.driver = webdriver.Firefox(executable_path=Locators.firefox_driver)
-        cls.driver.get(Locators.testServer + Locators.login_url)
-        cls.driver.maximize_window()
-        return cls.driver
+        drivers.append({"webdriver": webdriver.Firefox, "executable_path": Locators.firefox_driver})
+        drivers.append({"webdriver": webdriver.Chrome, "executable_path": Locators.chrome_driver})
+        drivers.append({"webdriver": webdriver.Edge, "executable_path": Locators.microsoft_edge_driver})
+        # return cls.driver
 
     def test_login(self):
-        driver = self.driver
-        driver.set_script_timeout(60)
-        namaste_username = "nelly@namaste.fit"
-        namaste_password = "qwertyui"
+        for browser in drivers:
+            self.driver = browser["webdriver"](executable_path=browser["executable_path"])
+            self.driver.get(Locators.testServer + Locators.login_url)
+            self.driver.maximize_window()
+            driver = self.driver
+            driver.set_script_timeout(60)
+            namaste_username = "nelly@namaste.fit"
+            namaste_password = "qwertyui"
 
-        email_field_id = "form-signin-email"
-        pass_field_id = "form-signin-password"
-        login_button_xpath = "//*[@id='formLogin']/button"
-        namaste_logo_xpath = "//*[@id='mainLogo']"
+            email_field_id = "form-signin-email"
+            pass_field_id = "form-signin-password"
+            login_button_xpath = "//*[@id='formLogin']/button"
+            namaste_logo_xpath = "//*[@id='mainLogo']"
 
-        email_field_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, email_field_id)))
-        pass_field_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, pass_field_id)))
-        login_button_element = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, login_button_xpath)))
+            email_field_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, email_field_id)))
+            pass_field_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, pass_field_id)))
+            login_button_element = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, login_button_xpath)))
 
-        email_field_element.clear()
-        email_field_element.send_keys(namaste_username)
+            email_field_element.clear()
+            email_field_element.send_keys(namaste_username)
 
-        pass_field_element.clear()
-        pass_field_element.send_keys(namaste_password)
+            pass_field_element.clear()
+            pass_field_element.send_keys(namaste_password)
 
-        login_button_element.click()
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, namaste_logo_xpath)))
+            login_button_element.click()
+            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, namaste_logo_xpath)))
 
-    def test_page_refresh(self):
-        WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, Locators.get_started_text_xpath)))
-        url = self.driver.current_url
-        self.driver.refresh()
+        # def test_page_refresh(self):
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, Locators.get_started_text_xpath)))
+            url = driver.current_url
+            driver.refresh()
 
-        # print(url)
-        time.sleep(3)
-        try:
-            print(url)
-            print(self.driver.current_url)
-            self.assertEqual(url, self.driver.current_url, "The page should return to " + url)
-            TestResults = "PASS"
-            print("1")
-            print(TestResults)
-            write_results(TestResults)
+            # print(url)
+            time.sleep(3)
+            try:
+                self.assertEqual(url, driver.current_url, "The page should return to " + url)
+                TestResults = "PASS"
+                sheetsUtils.write_results(TestResults, browser["webdriver"], user_story)
 
-        except Exception:
-            # var = traceback.format_exc()
-            # print(var)
-            TestResults = "FAIL"
-            print("2")
-            print(TestResults)
-            write_results(TestResults)
+            except Exception:
+                TestResults = "FAIL"
+                sheetsUtils.write_results(TestResults, browser["webdriver"], user_story)
 
-        # assertion = self.assertTrue(url, self.driver.current_url, "The page should return to " + url)
-        # print(assertion)
+            driver.close()
 
-    @classmethod
-    def tearDownClass(cls):
-        cls.driver.close()
-
-
-def write_results(TestResults):
-    written = False
-    print("3")
-    print(TestResults)
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(Locators.credentials, Locators.scope)
-    client = gspread.authorize(credentials)
-    max_rows = sheetsUtils.getRowCount(Locators.userStoriesTestCases, client)
-
-    print(max_rows)
-    for row in range(max_rows + 1):
-        print(row)
-        row_value = sheetsUtils.readData(Locators.userStoriesTestCases, client, row + 1, 1)
-        if row_value == "107":
-            sheetsUtils.writeData(Locators.userStoriesTestCases, client, row + 1, 2, TestResults)
-            written = True
-
-    if written == False:
-        new_row = ["107", TestResults, "", "", ""]
-        sheetsUtils.appendData(Locators.userStoriesTestCases, client, new_row)
+    # @classmethod
+    # def tearDownClass(cls):
+    #     cls.driver.close()
 
 
 if __name__ == '__main_':
